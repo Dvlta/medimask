da16c22e550e1adbcd6694c61ecf8b505ef9c209
Leo Sun <leobluehills2015@gmail.com>
2026-04-25 00:50:48 -0700

    Initial Commit

---
+++ medimask/medimaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  medimaskApp.swift
+//  medimask
+//
+//  Created by Leo Sun on 4/25/26.
+//
+
+import SwiftUI
+
+@main
+struct medimaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ medimaskTests/medimaskTests.swift
@@ -0,0 +1,17 @@
+//
+//  medimaskTests.swift
+//  medimaskTests
+//
+//  Created by Leo Sun on 4/25/26.
+//
+
+import Testing
+@testable import medimask
+
+struct medimaskTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
